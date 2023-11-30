56cbe476012f3f8814800513e03eee97bc4172c4
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-30 00:54:53 -0300

    refactor: moved validations of Username value object to Username.of() static constructor

--- server/src/remembering/domain/model/user/username.ts
+++ server/src/remembering/domain/model/user/username.ts
@@ -1,28 +1,30 @@
 import { Notification } from "../../../../util/types";
 
 export default class Username {
-    private readonly value: string
-    
-    constructor (
-        value: string,
-    ) {
-        this.value = value.toLowerCase()
-        
-        const notification = this.validate()
+    private constructor (
+        private readonly value: string
+    ) {}
+
+    static of(value: string) {
+        value = value.trim().toLowerCase()
+
+        const notification = Username.validate(value)
         if (notification.hasErrors()) throw new Error(notification.message)
+
+        return new Username(value)
     }
 
-    private validate() {
+    private static validate(value: string) {
         const notification = new Notification()
         
-        if (this.value.match(/[^a-z0-9._-]*/)) notification.addError(
-            `The username cannot contains symbols but letters, digits, periods, underscores and dashes! Provided: ${this.value}`
+        if (value.match(/[^a-z0-9._-]*/)) notification.addError(
+            `The username cannot contains symbols but letters, digits, periods, underscores and dashes! Provided: ${value}`
         )
-        if (this.value.match(/[._-][^a-z0-9]*/)) notification.addError(
-            `Periods, underscores and dashes must be followed by digits or letters in the username. Provided: ${this.value}`
+        if (value.match(/[._-][^a-z0-9]*/)) notification.addError(
+            `Periods, underscores and dashes must be followed by digits or letters in the username. Provided: ${value}`
         )
-        if (this.value.match(/^[^a-z]+/)) notification.addError(
-            `The username must begin with a letter! Provided: ${this.value}`
+        if (value.match(/^[^a-z]+/)) notification.addError(
+            `The username must begin with a letter! Provided: ${value}`
         )
 
         return notification
