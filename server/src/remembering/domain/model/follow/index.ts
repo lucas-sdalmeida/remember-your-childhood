d343f21b48b92c4c1db46ea53931acd1b33c33e7
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-12-02 17:13:00 -0300

    feat: added missing exports in src/remembering/domain/model/follow/

---
+++ server/src/remembering/domain/model/follow/index.ts
@@ -0,0 +1,5 @@
+import FollowRequest from "./follow-request";
+import FollowRequestId from "./request-id";
+import RequestStatus from "./request-status";
+
+export { FollowRequest, FollowRequestId, RequestStatus }
