af87517b3dbdb5336ef7c1db0a0618092b0cb436
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-12-05 18:30:05 -0300

    style: improved remembering/index.ts organization

--- server/src/remembering/index.ts
+++ server/src/remembering/index.ts
@@ -1,4 +1,15 @@
-import { CreateUserService, RequestModel as CreateUserRequestModel } from "./application/user/create";
+import SignUpServideImpl from "./application/session/signup/v1/signup-service-impl";
+
+import { RequestModel as CreateUserRequestModel } from "./application/user/create";
+import CreateUserServiceImpl from "./application/user/create/v1/create-user-service-impl";
+
 import { UUIDGenerator } from "./domain/services";
 
-export { CreateUserService, CreateUserRequestModel, UUIDGenerator }
+export {
+    SignUpServideImpl,
+
+    CreateUserServiceImpl,
+    CreateUserRequestModel, 
+
+    UUIDGenerator,
+}
